f34193b06604d1c8418cb273c3ec84e34610efae
Eleacid López Valle <ele.leader@gmail.com>
2019-01-20 20:38:03 -0600

    template sin rutas, creacion de la pagina plana de inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './shared/menu/menu.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { PrincipalComponent } from './pages/principal/principal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule
